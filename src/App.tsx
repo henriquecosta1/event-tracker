d6356888ab9156b8ccd7f956ff2d548196a9fa4c
Henrique Costa <contato.henrique19@gmail.com>
2025-05-15 09:01:37 -0300

    endpoint eventos mockado json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import style from './App.module.scss';
 import Card from './components/Card';
 import Formulario from './components/Formulario';
@@ -10,22 +10,24 @@ import { RecoilRoot } from 'recoil';
 function App() {
   return (
     <RecoilRoot>
-      <div className={style.App}>
-        <div className={style.Coluna}>
-          <Card>
-            <Formulario />
-          </Card>
-          <hr />
-          <Card>
-            <ListaDeEventos />
-          </Card>
+      <Suspense fallback="Está carregando">
+        <div className={style.App}>
+          <div className={style.Coluna}>
+            <Card>
+              <Formulario />
+            </Card>
+            <hr />
+            <Card>
+              <ListaDeEventos />
+            </Card>
+          </div>
+          <div className={style.Coluna}>
+            <Calendario />
+          </div>
         </div>
-        <div className={style.Coluna}>
-          <Calendario />
-        </div>
-      </div>
+      </Suspense>
     </RecoilRoot>
-    );
+  );
 }
 
 export default App;
--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,24 +1,11 @@
 import {atom} from 'recoil';
 import { IEvento } from '../interfaces/IEvento';
 import { IFiltroDeEventos } from '../interfaces/IFiltroDeEventos';
+import { eventosAsync } from './seletores';
 
 export const listaDeEventosState = atom<IEvento[]>({
     key: 'listaDeEventosState',
-    default: [{
-            "descricao": "Estudar React",
-            "inicio": new Date("2025-05-09T01:49"),
-            "fim": new Date("2025-05-09T05:49"),
-            "completo": false,
-            "id": 1642342747
-        },
-        {
-            "descricao": "Estudar Recoil",
-            "inicio": new Date("2025-05-08T03:16"),
-            "fim": new Date("2025-05-08T06:16"),
-            "completo": false,
-            "id": 1642342959
-        }
-    ]
+    default: eventosAsync
 })
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
